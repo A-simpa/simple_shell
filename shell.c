5310d65a161ec4423f90477a90866f6a91cbdc05
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-11-09 18:17:38 +0000

    first version

---
+++ shell.c
@@ -0,0 +1,74 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <signal.h>
+
+extern char **environ;
+
+
+/**
+ * main - execve example
+ *
+ * Return: Always 0.
+ */
+
+
+int main(int ac, int av)
+{
+	return (forking());
+}
+
+/**
+ * forking - create a new everytime
+ *
+ *
+ * Return: Always 0
+ */
+
+
+int forking(void)
+{
+	pid_t child_pid;
+	int status;
+	char **splited, *line = NULL;
+	size_t n = 0;
+	ssize_t char_read;
+
+	if (2 == 3)
+	{
+		return -1;
+	}
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		return(1);
+	}
+	else if (child_pid == 0)
+	{
+        	printf("#cisfun$ ");
+        	char_read = getline(&line, &n, stdin);
+		if (char_read == -1)
+			kill(0, 2);
+        	if (line[char_read - 1] == '\n')
+                	line[char_read - 1] = '\0';
+        	/*if (line[0] == 'e')
+        	 *{
+                 *	exit(0);
+        	}*/
+        	splited = _strtok(line);
+        	if (execve(splited[0], splited, environ) == -1)
+        	{
+                	printf("./shell: No such file or directory\n");
+        	}
+
+	}
+	else
+	{
+
+		wait(&status);
+		forking();
+	}
+}
---
+++ strtok.c
@@ -0,0 +1,95 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * count_words - count the number of word in a string
+ * @str: the pointer to the string
+ *
+ * Return: int of count (sucess)
+ */
+
+int count_words(char *str)
+{
+	int count = 0;
+
+	for (; *str != '\0'; str++)
+	{
+		if  (*str != ' ' && (*(str - 1) == ' ' || count == 0))
+		{
+			count++;
+		}
+	}
+	return (count);
+}
+
+/**
+ * in_len - get the length of all words in a string
+ * @str: the string pointer
+ *
+ * Return: the array length of the words
+ */
+
+int in_len(char *str)
+{
+	int len;
+
+	len = 0;
+	while (str[len] != ' ' && str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+
+}
+
+/**
+ * strtow - split a string into words
+ * @str: string to split
+ *
+ * Return: pointer to a pointer
+ */
+
+
+char **_strtok(char *str)
+{
+	char **strtwo;
+	int out_index = 0, ind = 0, len;
+
+	if (str == NULL || str[0] == '\0')
+	{
+		return (NULL);
+	}
+	strtwo = malloc((sizeof(char *)) * (count_words(str) + 1));
+	if (strtwo == NULL || count_words(str) == 0)
+	{
+		free(strtwo);
+		return (NULL);
+	}
+
+	for (; *str != '\0'; str++)
+	{
+		if (*str != ' ' && (*(str - 1) == ' ' || out_index == 0))
+		{
+
+			strtwo[ind] = malloc(sizeof(char) * (in_len(str) + 1));
+			if (strtwo[ind] == NULL)
+			{
+				for (; ind >= 0; ind--)
+					free(strtwo[ind]);
+				free(strtwo);
+				return (NULL);
+			}
+
+			len = 0;
+			while (str[len] != ' ' && str[len] != '\0')
+			{
+				strtwo[out_index][len] = str[len], len++;
+			}
+			strtwo[out_index][len] = '\0';
+			out_index++, ind++;
+		}
+	}
+	strtwo[out_index] = NULL;
+	return (strtwo);
+}
