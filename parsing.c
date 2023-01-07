704bfffd47293829de7fe0153fb15868a0af049f
A-simpa <mohammedabdulmujeeb16@gmail.com>
2023-01-07 19:09:00 +0000

    if path does not exist don run pwd

--- parsing.c
+++ parsing.c
@@ -10,13 +10,15 @@
 
 char **parsing(char *lineptr, char *name, int count, int *flag)
 {
-	char **arr;
+	char **arr, path[] = "PATH";
 	char  *cmd;
 	struct stat sb;
 
 	arr = split_words(lineptr, ' ');
 	if (arr)
 	{
+		if (_getenv(path) == NULL)
+			return(NULL);
 		if (stat(arr[0], &sb) == 0)
 			return (arr);
 		cmd = _which(arr[0]);
